8f29a0cb2eb8d4e9b2052305b4008312c6c8285e
trench009 <amennajam7@gmail.com>
2022-09-19 19:38:56 +0500

    add these all

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeadComponent } from './Netflix/head/head/head.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'head',component:HeadComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeadComponent } from './Netflix/head/head/head.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    HeadComponent
   ],
   imports: [
     BrowserModule,
